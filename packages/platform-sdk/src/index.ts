d079fb51fc979934a0ef2f0029070c6770835a9d
Zeljko <zeljko89markovic@gmail.com>
2023-07-24 11:00:46 +0200

    Allow instantiating platform sdk from Platform Actions

--- packages/platform-sdk/src/index.ts
+++ packages/platform-sdk/src/index.ts
@@ -4,18 +4,27 @@ import { RelayClient } from '@openzeppelin/platform-sdk-relay-client';
 import { ProposalClient } from '@openzeppelin/platform-sdk-proposal-client';
 import { DeployClient } from '@openzeppelin/platform-sdk-deploy-client';
 import { NotificationChannelClient } from '@openzeppelin/platform-sdk-notification-channel-client';
-import { RelaySignerClient } from '@openzeppelin/platform-sdk-relay-signer-client';
 import { Newable, ClientParams } from './types';
+import { ActionRelayerParams, Relayer as RelaySignerClient } from '@openzeppelin/platform-sdk-relay-signer-client';
 
 interface PlatformOptions {
   apiKey?: string;
   apiSecret?: string;
   relayerApiKey?: string;
   relayerApiSecret?: string;
+  credentials?: ActionRelayerParams;
+  relayerARN?: string;
 }
 
-function getClient<T>(Client: Newable<T>, credentials: Partial<ClientParams>): T {
-  if (!credentials.apiKey || !credentials.apiSecret) throw new Error(`API key and secret are required`);
+function getClient<T>(Client: Newable<T>, credentials: Partial<ClientParams> | ActionRelayerParams): T {
+  console.log('creds', credentials);
+  if (
+    !('credentials' in credentials && 'relayerARN' in credentials) &&
+    !('apiKey' in credentials && 'apiSecret' in credentials)
+  ) {
+    throw new Error(`API key and secret are required`);
+  }
+
   return new Client(credentials);
 }
 
@@ -24,12 +33,17 @@ export class Platform {
   private apiSecret: string | undefined;
   private relayerApiKey: string | undefined;
   private relayerApiSecret: string | undefined;
+  private actionCredentials: ActionRelayerParams | undefined;
+  private actionRelayerArn: string | undefined;
 
   constructor(options: PlatformOptions) {
     this.apiKey = options.apiKey;
     this.apiSecret = options.apiSecret;
     this.relayerApiKey = options.relayerApiKey;
     this.relayerApiSecret = options.relayerApiSecret;
+    // support for using relaySigner from Platform Actions
+    this.actionCredentials = options.credentials;
+    this.actionRelayerArn = options.relayerARN;
   }
 
   get monitor() {
@@ -52,11 +66,16 @@ export class Platform {
     return getClient(DeployClient, { apiKey: this.apiKey, apiSecret: this.apiSecret });
   }
 
-  get notifiationChannel() {
+  get notificationChannel() {
     return getClient(NotificationChannelClient, { apiKey: this.apiKey, apiSecret: this.apiSecret });
   }
 
   get relaySigner() {
-    return getClient(RelaySignerClient, { apiKey: this.relayerApiKey, apiSecret: this.relayerApiSecret });
+    return getClient(RelaySignerClient, {
+      ...(this.actionCredentials ? { credentials: this.actionCredentials } : undefined),
+      ...(this.actionRelayerArn ? { relayerARN: this.actionRelayerArn } : undefined),
+      ...(this.relayerApiKey ? { apiKey: this.relayerApiKey } : undefined),
+      ...(this.relayerApiSecret ? { apiSecret: this.relayerApiSecret } : undefined),
+    });
   }
 }
