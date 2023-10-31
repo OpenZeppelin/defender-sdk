c44504874bb80740bb96a2cbab9a35e5f67af4a3
ShahNami <me@nami.sh>
2023-10-31 16:41:38 +0400

    Export types

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,7 +1,13 @@
 export { ActionClient } from './api';
-export { CreateActionRequest, UpdateActionRequest, GetSecretsResponse, SaveSecretsRequest } from './models/action';
+export {
+  CreateActionRequest,
+  UpdateActionRequest,
+  GetSecretsResponse,
+  SaveSecretsRequest,
+  EnvironmentVariables,
+} from './models/action';
 export { ActionRunBase, ActionRunListResponse, ActionRunResponse } from './models/action-run.res';
-export { ActionDeleteResponse, ActionListResponse, ActionResponse } from './models/response';
+export { ActionDeleteResponse, ActionListResponse, ActionResponse, ActionMessageResponse } from './models/response';
 export * from './models/action-event-utils';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
