d1e99b35b7da6984a765f77cb10bc7d3430f80b2
ShahNami <me@nami.sh>
2023-11-30 16:58:45 +0400

    Update models

--- packages/network/src/models/networks.ts
+++ packages/network/src/models/networks.ts
@@ -2,14 +2,16 @@ import { SupportedNetwork, TenantNetwork } from '@openzeppelin/defender-sdk-base
 
 export type NetworkType = 'production' | 'test';
 export type TenantNetworkType = 'private' | 'fork';
+export type Address = string;
 
 export interface ListNetworkRequestOptions {
   networkType?: NetworkType;
 }
 
 export interface TenantNetworkCreateRequest {
-  name: TenantNetwork;
-  supportedNetwork: SupportedNetwork;
+  name: string;
+  supportedNetwork?: SupportedNetwork;
+  configuration?: TenantNetworkConfiguration;
   rpcUrl: string;
   blockExplorerUrl?: string;
   networkType: TenantNetworkType;
@@ -21,6 +23,7 @@ export interface TenantNetworkUpdateRequest {
   tenantNetworkId: string;
   apiKey?: string;
   blockExplorerUrl?: string;
+  configuration?: TenantNetworkConfiguration;
   stackResourceId?: string;
 }
 
@@ -28,12 +31,29 @@ export interface TenantNetworkResponse {
   tenantNetworkId: string;
   name: TenantNetwork;
   chainId: number;
-  supportedNetwork: SupportedNetwork;
+  supportedNetwork?: SupportedNetwork;
   rpcUrl: string;
   apiKey?: string;
   blockExplorerUrl?: string;
   stackResourceId?: string;
   networkType: TenantNetworkType;
+  configuration?: TenantNetworkConfiguration;
   createdAt: string;
   createdBy: string;
+}
+
+export interface TenantNetworkConfiguration {
+  symbol: string;
+  eips?: TenantNetworkEIPConfiguration;
+  safeContracts?: SafeContracts;
+  subgraphURL?: string;
+}
+export interface TenantNetworkEIPConfiguration {
+  isEIP1559?: boolean;
+}
+export interface SafeContracts {
+  master: Address;
+  proxyFactory: Address;
+  multisendCallOnly: Address;
+  createCall?: Address;
 }
