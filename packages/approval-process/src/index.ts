817f654c657f570f93d394d6134b57b59fbd728c
Marcos <marcoscarlomagno1@gmail.com>
2024-02-28 21:52:38 -0300

    expose approval process response

--- packages/approval-process/src/index.ts
+++ packages/approval-process/src/index.ts
@@ -1,5 +1,5 @@
 export { ApprovalProcessClient } from './api';
-export { CreateApprovalProcessRequest } from './models/approval-process';
+export { CreateApprovalProcessRequest, CreateApprovalProcessResponse } from './models/approval-process';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const VERSION = require('../package.json').version;
