03c78e37265d59d3245f3266b96e4a738a9cf794
collins-w <delanhype@gmail.com>
2024-05-24 14:10:13 +0300

    Fixing the release script - tagging

--- scripts/release.js
+++ scripts/release.js
@@ -25,7 +25,7 @@ process.chdir(path.join(__dirname, ".."));
     throw new Error(`git ls-remote exited with ${exitCode}:\n${stderr}`);
   }
 
-  await exec("changeset", ["tag"]);
+  await exec("changeset");
 
   // await exec("git", [
   //   "push",
