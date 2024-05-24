304b08b7e078e1bd34d7e51ac8aeb03ea658df5f
collins-w <delanhype@gmail.com>
2024-05-24 14:03:30 +0300

    Fixing the release script

--- scripts/release.js
+++ scripts/release.js
@@ -25,10 +25,6 @@ process.chdir(path.join(__dirname, ".."));
     throw new Error(`git ls-remote exited with ${exitCode}:\n${stderr}`);
   }
 
-  await exec("git", ["checkout", "--detach"]);
-  await exec("git", ["add", "--force", "dist"]);
-  await exec("git", ["commit", "-m", tag]);
-
   await exec("changeset", ["tag"]);
 
   await exec("git", [
