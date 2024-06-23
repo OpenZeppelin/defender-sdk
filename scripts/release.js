cd4bbaf04b1a069777d6668c8290886751519d0e
collins-w <delanhype@gmail.com>
2024-06-23 20:55:59 +0300

    Update the script

--- scripts/release.js
+++ scripts/release.js
@@ -25,7 +25,7 @@ process.chdir(path.join(__dirname, ".."));
     throw new Error(`git ls-remote exited with ${exitCode}:\n${stderr}`);
   }
 
-  await exec("npm", ["publish --dry-run"]);
+  await exec("npm publish --dry-run");
   
   // await exec("changeset", ["tag"]);
 
