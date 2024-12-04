dc4ad4ae4f4f85861d209a8abf8cb2c70b31c1ab
shahnami <me@nami.sh>
2024-12-04 15:37:57 +0400

    feat(sim): update sim models (breaking change)

--- packages/proposal/src/models/simulation.ts
+++ packages/proposal/src/models/simulation.ts
@@ -1,97 +1,98 @@
 // Copied from openzeppelin/defender/models/src/types/simulation.res.d.ts
 
-export type Address = string;
-export type EventArgs = EventArgValues[];
-/**
- * @minItems 3
- */
-export type Log = [
-  Address | string[] | string,
-  Address | string[] | string,
-  Address | string[] | string,
-  ...(Address | string[] | string)[],
-];
-export type BigUInt = string | string | number;
 
-export interface SimulationResponse {
-  contractProposalId: string;
-  createdAt: string;
-  transaction: SimulationTransaction;
-  meta: SimulationMetadata;
-  states: ContractState[];
-  events: LogEvent[];
-  logs?: Log[];
-  storage: StorageState[];
-  traces: TransactionTrace[];
-  transfers: TransactionTrace[];
-}
-export interface SimulationTransaction {
-  to: string;
-  from: string;
-  data: string;
-  value: string;
-  nonce: number;
-  gasPrice: number;
-  gasLimit?: number;
-}
-export interface SimulationMetadata {
-  network: string;
-  blockNumber: number;
-  forkedBlockNumber: number;
-  simulatedBlockNumber: number;
-  gasUsed: number;
-  returnValue: string;
-  returnString: string;
-  reverted: boolean;
-}
-export interface ContractState {
-  function: string;
-  types: string[];
-  states: {
-    previous: string;
-    current: string;
-  };
-}
-export interface LogEvent {
-  address: Address;
-  name: string;
-  signature: string;
-  topics?: string[];
-  args: EventArgs;
-}
-export interface EventArgValues {
-  name?: string;
-  type?: string;
-  indexed?: boolean;
-  value: string | Record<string, unknown>;
-}
-export interface StorageState {
-  address: Address;
-  slot: string;
-  states: {
-    previous?: string;
-    current: string;
-  };
-}
-export interface TransactionTrace {
-  depth: number;
-  type: string;
-  gas: BigUInt;
-  to: string;
-  value: BigUInt;
-  data: string;
-}
-
-// Copied from openzeppelin/defender/models/src/types/simulation.req.d.ts
+export type BigUInt = string | number;
 
 export interface SimulationRequest {
   blockNumber?: BigUInt;
-  transactionData: TransactionData;
+  transactionData: Transaction;
 }
-export interface TransactionData {
-  to: Address;
-  from?: Address;
-  value: BigUInt;
+export interface Transaction {
+  from?: string;
+  to: string;
   data: string;
-  [k: string]: unknown;
+  value: BigUInt;
+  gasPrice?: number;
+  gas?: number;
+  nonce?: number;
+  maxFeePerGas?: number;
+  maxPriorityFeePerGas?: number;
+}
+
+
+export interface SimulationResponse {
+  summary: {
+    network: string;
+    blockNumber: number;
+    totalCalls: number;
+    cummulativeGasUsed: number;
+    reverted: boolean;
+    result?: string;
+    revertReason?: string;
+    events: IEvent[];
+    contractStates: ContractState[];
+    states: TraceRecordStateDiff[];
+    transfers: TraceRecordTransaction[];
+  };
+  records: {
+    index: number;
+    transaction: TraceRecordTransaction;
+    events?: IEvent[];
+    states?: TraceRecordStateDiff[];
+    status: {
+      reverted: boolean;
+      gasUsed: string;
+      returnValue: string;
+    };
+    revertReason?: string;
+  }[];
+  contractProposalId: string;
+  createdAt: string;
+  error?: string;
+}
+export interface IEvent {
+  address?: string;
+  name: string;
+  signature: string;
+  topics: string[];
+  args: EventArg[];
+}
+export interface EventArg {
+  value: string | number | boolean | {};
+  name: string;
+  indexed: boolean;
+  type: string;
+}
+export interface ContractState {
+  address: string;
+  states: {
+    [k: string]: ContractStateValue;
+  };
+}
+export interface ContractStateValue {
+  types: string[];
+  pre: string;
+  post: string;
+  changed: boolean;
+}
+export interface TraceRecordStateDiff {
+  address: string;
+  key: string;
+  pre: string;
+  post: string;
+  changed: boolean;
+}
+export interface TraceRecordTransaction {
+  type: 'CALL' | 'DELEGATECALL' | 'STATICCALL';
+  data: string;
+  from: string;
+  to: string;
+  gas?: string;
+  gasPrice?: string;
+  value?: string;
+  nonce?: string;
+  blockNumber?: string;
+  description?: {
+    [k: string]: unknown;
+  };
 }
