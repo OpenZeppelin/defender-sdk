8c312a3ed7253b33bc8193ac54aa3648a62d9813
dylankilkenny <dylankilkenny95@gmail.com>
2025-02-21 22:11:36 +0000

    feat: Add Abstract support (#685)
    Signed-off-by: dylankilkenny <dylankilkenny95@gmail.com>

--- packages/base/src/utils/network.ts
+++ packages/base/src/utils/network.ts
@@ -5,6 +5,8 @@ export type SupportedNetwork = PublicNetwork | CustomNetwork;
 export type PublicNetwork =
   | 'alfajores'
   | 'amoy'
+  | 'abstract'
+  | 'abstract-sepolia'
   | 'arbitrum-nova'
   | 'arbitrum-sepolia'
   | 'arbitrum'
@@ -61,6 +63,8 @@ export type TenantNetwork = string;
 export const Networks: Network[] = [
   'alfajores',
   'amoy',
+  'abstract',
+  'abstract-sepolia',
   'arbitrum-nova',
   'arbitrum-sepolia',
   'arbitrum',
@@ -131,6 +135,8 @@ export function toChainId(network: Network): number | undefined {
 export const chainIds: { [key in Network]: number } = {
   'alfajores': 44787,
   'amoy': 80002,
+  'abstract': 2741,
+  'abstract-sepolia': 11124,
   'arbitrum': 42161,
   'arbitrum-nova': 42170,
   'arbitrum-sepolia': 421614,
